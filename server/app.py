be8cf833c9b3191c19419844d632b912eff255a0
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-04 15:45:50 +0300

    Flask- sqlAlchemy code-along

--- server/app.py
+++ server/app.py
@@ -1,25 +1,19 @@
-# server/app.py
-
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
-# create a Flask application instance
-app = Flask(__name__)
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+app.config ['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
-# configure the database connection to the local file app.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+#create a migration object to manage schema modifications
+migrate=Migrate(app, db)
 
-# configure flag to disable modification tracking and use less memory
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# create a Migrate object to manage schema modifications
-migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
+#initialize the flask app to use the db
 db.init_app(app)
 
 
-if __name__ == '__main__':
+
+
+if __name__=='__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,18 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
+metadata=MetaData()
 
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
+#creating a Flask SQLAlchemy extension
+db=SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
-
+#define a model class by inheriting from db.model
 
 class Pet(db.Model):
-    __tablename__ = 'pets'
+    __tablename__='pets'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    species=db.Column(db.String)
